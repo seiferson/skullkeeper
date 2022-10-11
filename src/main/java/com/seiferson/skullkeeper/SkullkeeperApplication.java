189dcb3628db033d032543dcb2cf38b9246e73b6
Cuauhtemoc Herrera <seifer.ch@gmail.com>
2022-10-10 22:34:06 -0500

    Added base spring files

---
+++ src/main/java/com/seiferson/skullkeeper/SkullkeeperApplication.java
@@ -0,0 +1,13 @@
+package com.seiferson.skullkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkullkeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkullkeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seiferson/skullkeeper/SkullkeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seiferson.skullkeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkullkeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
